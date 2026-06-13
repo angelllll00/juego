fe649a575bde4a0c1025d9124f611410a32eb283
angelllll00 <angellezama0045@gmail.com>
2026-06-13 13:56:49 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
